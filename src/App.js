ee95d93be059c7f74b1c77682722a3e490c43055
Quang <0348711727>
2021-03-15 09:00:29 +0700

    full source

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {Button} from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hallo</h1>
+      <button>Bấm đi</button>
+      <Button>Bấm luôn</Button>
+    </div>
+  );
+}
+
+export default App;
